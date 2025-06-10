4d31f1da593bdeec591c9f352520e1c168ee5a9b
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-11 02:30:11 +0530

    a lot to grab but as of now parcel is very strong

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-  
-  const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"tag"},"i am an h1 tag"),React.createElement("h2",{},"i am an h2 tag")]))
-  
-  const root=ReactDOM.createRoot(document.getElementById("parent"));
-
-  root.render(parent);
-
-  //this is very complex thats why jsk was introduced
-  
-  
-  
-  
-  
-//   const heading=React.createElement("h1",{id:"heading" , class:"head"},"hello world from react");
-
-//         const root=ReactDOM.createRoot(document.getElementById("root"));
-
-//         root.render(heading);
+import React from "react";
+import { createRoot } from "react-dom/client"; 
 
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "tag", key: "h1" }, "I am an h tag"),
+    React.createElement("h2", { key: "h2" }, "I am an h2 tag"),
+  ])
+);
 
-        
 
-        
+const rootElement = document.getElementById("root"); // ✅ must match the div in HTML
+const root = createRoot(rootElement); // ✅ works only in React 18+
+root.render(parent);
