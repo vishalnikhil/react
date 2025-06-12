6d48fd4d60e0fa2b02264bf365128afb86438c41
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-12 22:42:12 +0530

    jsx component

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; 
 
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "tag", key: "h1" }, "I am an h tag"),
-    React.createElement("h2", { key: "h2" }, "I am an h2 tag"),
-  ])
+import ReactDOM from "react-dom/client"; 
+
+// const parent=React.createElement("h1",{id:"parent"},"this is direct from React");
+
+const Component= ()=>(
+
+       <div id="mainContainer">
+
+   <h1 id="comp"> this is jsk component</h1>
+
+
+       </div>
+
 );
 
+//what to do noq
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-const rootElement = document.getElementById("root"); // ✅ must match the div in HTML
-const root = createRoot(rootElement); // ✅ works only in React 18+
-root.render(parent);
+root.render(<Component/>);
+
