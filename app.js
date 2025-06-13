c90503c8221cae4ca7b51c32e4b019c8f6b17503
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-14 01:43:23 +0530

    ----

--- app.js
+++ app.js
@@ -3,21 +3,136 @@ import React from "react";
 
 import ReactDOM from "react-dom/client"; 
 
-// const parent=React.createElement("h1",{id:"parent"},"this is direct from React");
 
-const Component= ()=>(
+const Header= ()=>{
 
-       <div id="mainContainer">
+     return(
+           
+          <div className="header">
 
-   <h1 id="comp"> this is jsk component</h1>
+            {/* logo */}
+
+            <div className="logo">
+
+                  <img className="img-logo" src="https://marketplace.canva.com/EAFaFUz4aKo/3/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-tn1zF-_cG9c.jpg"></img>
+
+            </div>
 
 
-       </div>
 
-);
+            {/* navbar */}
 
-//what to do noq
-const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Component/>);
+            <div className="navbar">
+
+                <ul className="navitems" >
+                     
+                    <li className="navlist">Home</li>
+                    <li className="navlist">About us</li>
+                    <li className="navlist">Contact us</li>
+                    <li className="navlist">Cart</li>
+                    
+                </ul>
+
+
+            </div>
+
+
+          </div>
+     )
+
+}
+
+  const Stylecard={
+       background:"#fff7e3"
+  }
+
+ const RestCard=()=>{
+       return(
+            <div className="rescard" style={Stylecard}>
+
+           
+                  <img className="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC_HBGeynBdWgXk3jz4jvZykhECPo0BVI1_Q&s"></img>
+
+                 <h3 className="resname">KFC</h3>
+                 <h4>Chicken,Non veg,fries</h4>
+                 <h4>4 stars</h4>
+                 <h4>29 minutes</h4>
+
+            </div>
+       )
+ }
+
+ const Body=()=>{
+
+     return(
+
+           <div className="body">
+
+
+                <div className="search">
+                    <input placeholder="search food items"></input>
+                    <button>enter</button>
+                </div>
+
+
+                <div className="rest-container">
+
+
+                    <RestCard/>
+                     <RestCard/>
+                      <RestCard/>
+                       <RestCard/>
+                       <RestCard/>
+                       <RestCard/>
+                       <RestCard/>
+                       
+
+
+
+
+
+
+                </div>
+
+             
+   
+
+
+           </div>
+
+     )
+       
+      
+ }
+
+
+  const Applayout=()=>{
+
+     return(
+          <div className=""> 
+
+
+            <Header/>
+            <Body></Body>
+           </div>
+     )
+
+  };
+
+
+  const root=ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(<Applayout/>)
+
+
+ 
+
+
+
+
+
+
+
+
 
